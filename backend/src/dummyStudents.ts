2d97a7475bbd9d1a45058d22571fe4aae6ebd19e
Amalie <iagresho@hiof.no>
2024-09-26 12:09:01 +0200

    Create dummyStudents.ts

---
+++ backend/src/dummyStudents.ts
@@ -0,0 +1,10 @@
+// dummyStudents.ts
+import { Student as StudentProps } from "./Types"; // Importer typen du har definert for en student
+
+export const dummyStudents: StudentProps[] = [
+  { id: "1", name: "Ola Nordmann" },
+  { id: "2", name: "Kari Nordmann" },
+  { id: "3", name: "Per Hansen" },
+  { id: "4", name: "Anne Olsen" },
+  { id: "5", name: "Marius Berg" }
+];
